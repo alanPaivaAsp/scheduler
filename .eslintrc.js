d2d2f32e30208a23b582a006d88f23f97edaa05f
Alan <alan@contaswap.com>
2020-11-11 04:39:53 -0300

    Add vue linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'plugin:vue/essential',
+    'eslint:recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
